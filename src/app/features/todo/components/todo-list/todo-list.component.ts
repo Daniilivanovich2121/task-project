2901172d165a32fc00c19ad4d71c8f9ecc5ffbf4
пвпвавпвапва <pritulodanil@gmail.com>
2025-07-18 20:58:23 +0300

    сделал для каждой колонки свой observable, при запросе распределяются по статусу

--- src/app/features/todo/components/todo-list/todo-list.component.ts
+++ src/app/features/todo/components/todo-list/todo-list.component.ts
@@ -5,6 +5,10 @@ import {TodoCardComponent} from '../todo-card/todo-card.component';
 import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {CdkDropList} from '@angular/cdk/drag-drop';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {Todo} from '../../models/todo.model';
+import {map, Observable} from 'rxjs';
+
 
 @Component({
   selector: 'app-todo-list',
@@ -14,19 +18,29 @@ import {CdkDropList} from '@angular/cdk/drag-drop';
     AsyncPipe,
     MatIcon,
     MatButton,
-    CdkDropList
+    CdkDropList,
+    MatProgressSpinner,
+
   ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TodoListComponent implements OnInit{
+export class TodoListComponent implements OnInit {
   public readonly taskService = inject(TaskService);
-  public readonly tasks$ = this.taskService.todos$;
+  public readonly tasks$: Observable<Todo[]> = this.taskService.todos$;
 
+
+  public readonly incompleteTasks$ = this.tasks$.pipe(
+    map(tasks => tasks.filter(task => !task.completed))
+  );
+
+  public readonly completedTasks$ = this.tasks$.pipe(
+    map(tasks => tasks.filter(task => task.completed))
+  );
   ngOnInit() {
     this.taskService.getTask()
   }
 
-  protected readonly length = length;
+
 }
